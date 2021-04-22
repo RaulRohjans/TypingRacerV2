8222fca9973f5130c543bd04797fe861e5f61b98
RaulRohjans <77687494+RaulRohjans@users.noreply.github.com>
2021-04-22 20:37:48 +0100

    Project

---
+++ Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿
+namespace Typing_Racer_V2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txt_text = new System.Windows.Forms.TextBox();
+            this.txt_word = new System.Windows.Forms.TextBox();
+            this.btn_start = new System.Windows.Forms.Button();
+            this.btn_stop = new System.Windows.Forms.Button();
+            this.btn_leave = new System.Windows.Forms.Button();
+            this.lbl_time = new System.Windows.Forms.Label();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // txt_text
+            // 
+            this.txt_text.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txt_text.Font = new System.Drawing.Font("Lucida Console", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_text.Location = new System.Drawing.Point(12, 12);
+            this.txt_text.Multiline = true;
+            this.txt_text.Name = "txt_text";
+            this.txt_text.ReadOnly = true;
+            this.txt_text.Size = new System.Drawing.Size(1240, 392);
+            this.txt_text.TabIndex = 0;
+            // 
+            // txt_word
+            // 
+            this.txt_word.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txt_word.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_word.Location = new System.Drawing.Point(515, 473);
+            this.txt_word.Name = "txt_word";
+            this.txt_word.ReadOnly = true;
+            this.txt_word.Size = new System.Drawing.Size(237, 22);
+            this.txt_word.TabIndex = 1;
+            this.txt_word.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // btn_start
+            // 
+            this.btn_start.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_start.Location = new System.Drawing.Point(584, 510);
+            this.btn_start.Name = "btn_start";
+            this.btn_start.Size = new System.Drawing.Size(112, 23);
+            this.btn_start.TabIndex = 2;
+            this.btn_start.Text = "Start";
+            this.btn_start.UseVisualStyleBackColor = true;
+            this.btn_start.Click += new System.EventHandler(this.btn_start_Click);
+            // 
+            // btn_stop
+            // 
+            this.btn_stop.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_stop.Enabled = false;
+            this.btn_stop.Location = new System.Drawing.Point(584, 548);
+            this.btn_stop.Name = "btn_stop";
+            this.btn_stop.Size = new System.Drawing.Size(112, 23);
+            this.btn_stop.TabIndex = 3;
+            this.btn_stop.Text = "Stop";
+            this.btn_stop.UseVisualStyleBackColor = true;
+            this.btn_stop.Click += new System.EventHandler(this.btn_stop_Click);
+            // 
+            // btn_leave
+            // 
+            this.btn_leave.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_leave.Location = new System.Drawing.Point(1066, 641);
+            this.btn_leave.Name = "btn_leave";
+            this.btn_leave.Size = new System.Drawing.Size(186, 28);
+            this.btn_leave.TabIndex = 4;
+            this.btn_leave.Text = "Leave";
+            this.btn_leave.UseVisualStyleBackColor = true;
+            this.btn_leave.Click += new System.EventHandler(this.btn_leave_Click);
+            // 
+            // lbl_time
+            // 
+            this.lbl_time.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.lbl_time.AutoSize = true;
+            this.lbl_time.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_time.Location = new System.Drawing.Point(618, 419);
+            this.lbl_time.Name = "lbl_time";
+            this.lbl_time.Size = new System.Drawing.Size(44, 31);
+            this.lbl_time.TabIndex = 5;
+            this.lbl_time.Text = "60";
+            // 
+            // timer
+            // 
+            this.timer.Interval = 1000;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1264, 681);
+            this.Controls.Add(this.lbl_time);
+            this.Controls.Add(this.btn_leave);
+            this.Controls.Add(this.btn_stop);
+            this.Controls.Add(this.btn_start);
+            this.Controls.Add(this.txt_word);
+            this.Controls.Add(this.txt_text);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(1280, 720);
+            this.Name = "Form1";
+            this.Text = "Typing Racer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_text;
+        private System.Windows.Forms.TextBox txt_word;
+        private System.Windows.Forms.Button btn_start;
+        private System.Windows.Forms.Button btn_stop;
+        private System.Windows.Forms.Button btn_leave;
+        private System.Windows.Forms.Label lbl_time;
+        private System.Windows.Forms.Timer timer;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Typing_Racer_V2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private readonly int SECONDS = 60;
+
+        int wordCnt = 0;
+        string path = "";
+        string contentOG = "";
+        List<char> contentTemp = new List<char>();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("Welcome to the typing racer game, made by RAUL ROHJANS!\n\nEnjoy :)", "Typing Racer", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+        }
+        private void btn_start_Click(object sender, EventArgs e)
+        {
+            btn_start.Enabled = false;
+            btn_stop.Enabled = true;
+            txt_word.ReadOnly = false;
+            wordCnt = 0;
+            lbl_time.Text = SECONDS.ToString();
+
+            OpenFileDialog open = new OpenFileDialog();
+            open.Filter = "Text|*.txt|All|*.*";
+            open.RestoreDirectory = true;
+
+            if (open.ShowDialog() == DialogResult.OK)
+            {
+                path = open.FileName;
+                StreamReader reader = new StreamReader(open.OpenFile());
+                contentOG = reader.ReadToEnd();
+
+                string tempStr = contentOG.Replace('\r', ' ');
+                char[] Temp = tempStr.Replace('\n', ' ').ToCharArray();
+                
+                //Fill list
+                for(int i = 0; i < Temp.Length; i++)
+                {
+                    if (Temp[i] == ' ' && Temp[i + 1] == ' ')
+                        continue;
+
+                    contentTemp.Add(Temp[i]);
+                }
+
+                txt_text.Text = new string(contentTemp.ToArray());
+                timer.Enabled = true;
+            }
+            else
+            {
+                btn_start.Enabled = true;
+                btn_stop.Enabled = false;
+                txt_word.ReadOnly = true;
+            }
+
+        }
+
+        private void btn_leave_Click(object sender, EventArgs e)
+        {
+            DialogResult res = MessageBox.Show("Are you sure you want to leave?", "Leave Game?", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            if (res == DialogResult.Yes)
+                Application.Exit();
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            txt_word.BackColor = Color.White;
+            if (e.KeyChar == ' ')
+            {                
+                int idx = 0;
+                bool correct = true;
+                char[] tempchar = txt_word.Text.ToCharArray();
+                while(contentTemp[idx] != ' ')
+                {
+                    if(contentTemp[idx] != tempchar[idx])
+                    {
+                        txt_word.BackColor = Color.Red;
+                        correct = false;
+                        break;
+                    }
+                    idx++;
+                }
+
+                if(correct)
+                {
+                    txt_word.Text = "";
+
+                    //Remove word from List
+                    for(int i = 0; i <= idx; i++)
+                    {
+                        contentTemp.RemoveAt(0);
+                    }
+
+                    txt_text.Text = new string(contentTemp.ToArray());
+                    wordCnt++;
+                }
+
+                e.Handled = true;
+            }
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            if (lbl_time.Text != "0" && lbl_time.Text != "00")
+            {
+                lbl_time.Text = Convert.ToString(Convert.ToInt32(lbl_time.Text) - 1);
+            }
+            else
+            {
+                timer.Enabled = false;
+                lbl_time.Text = "0";
+
+                MessageBox.Show("The time is up!\n\nYou scored " + wordCnt / (SECONDS / 60.0) + " WPM (Words per Minute)", "Game Over", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                btn_start.Enabled = true;
+                btn_stop.Enabled = false;
+                txt_word.ReadOnly = true;
+            }
+        }
+
+        private void btn_stop_Click(object sender, EventArgs e)
+        {
+            btn_start.Enabled = true;
+            btn_stop.Enabled = false;
+            txt_word.ReadOnly = true;
+            MessageBox.Show("Aww, you canceled the game :(\n\nYou scored " + wordCnt / ((SECONDS - Convert.ToDouble(lbl_time.Text)) / 60.0) + " WPM (Words per Minute)", "Game Over", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            timer.Enabled = false;
+        }
+    }
+}
